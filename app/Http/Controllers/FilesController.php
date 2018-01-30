689e5df734f7746d59768648bc14dc669566494d
LevFlavien <levesque.flavien@outlook.fr>
2018-01-30 19:00:27 +0100

    Ajout fonction suppression fichier.

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
+use App\Group;
+use App\File;
 
 class FilesController extends Controller
 {
     public function index() {
 
+    }
+
+    public function create(Group $group) {
+
+        return view('files.form', compact('group'));
     }
 
     /**
@@ -17,17 +24,31 @@ class FilesController extends Controller
      * @param Group $group
      * @param File $file
      */
-    public function store(Client $client, Group $group, File $file) {
+    public function upload(Client $client, Group $group, File $file) {
 
         $client->setDefaultOption('verify', false);
-        $reponse = $client->post('https://content.dropboxapi.com/2/files/upload', [
+        $response = $client->post('https://content.dropboxapi.com/2/files/upload', [
             'headers' => [
                 'Authorization' => 'Bearer ' . getenv('DROPBOX_API_KEY', 'null'),
                 'Content-Type' => 'application/octet-stream',
-                'Dropbox-API-Arg' => '{"path":"' . $group->id . '/' . $file->name . '"}'
+                'Dropbox-API-Arg' => '{"path":"/' . $group->id . '/' . $file->name . '"}'
             ]
         ]);
 
-        return $reponse->getBody();
+        return $response->getBody();
+    }
+
+    public function delete(Client $client, Group $group, File $file) {
+
+        $client->setDefaultOption('verify', false);
+        $response = $client->post('https://api.dropboxapi.com/2/files/delete_v2', [
+            'headers' => [
+                'Authorization' => 'Bearer ' . getenv('DROPBOX_API_KEY', 'null'),
+                'Content-Type' => 'application/json',
+            ],
+            'body' => '{"path":"/' . $group->id . '/' . $file->name . '"}'
+        ]);
+
+        return $response->getBody();
     }
 }
--- resources/views/files/form.blade.php
+++ resources/views/files/form.blade.php
@@ -5,11 +5,15 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Groupes</div>
+                    <div class="panel-heading">Ajout fichier</div>
 
                     <div class="panel-body">
 
-                        {!! BootForm::open(['model' => $file ?? new App\File(), 'store' => 'files.store', 'update' => 'files.update']) !!}
+                        {!! BootForm::open(['model' => $file ?? new App\File(), 'url' => route('files.upload', ['group' => $group->id]), 'method' => 'POST']) !!}
+
+                            {!! BootForm::file('name') !!}
+
+                            {!! BootForm::submit() !!}
 
                         {!! BootForm::close() !!}
 
